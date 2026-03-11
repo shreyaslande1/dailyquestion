4a946d274a2f4119da9587ec931f2fb27ed83a4b
shreyaslande <shreyaslande200@gmail.com>
2026-03-11 22:50:03 +0530

    fibonacci

--- fibonacci.java
+++ fibonacci.java
@@ -5,18 +5,17 @@ public class fibonacci {
         Scanner in = new Scanner(System.in);
         System.out.print("enter the number you want to print fibonacci series up to that number: ");
         int n = in.nextInt();
-        // fibo(n);
+        fibo(n);
 
         //fibonacci series using reccursioon
-        for(int i=0;i<n;i++){
-            int a = fibousingrec(i);
-            System.out.print(a+" ");
-        }
+        // for(int i=0;i<n;i++){
+        //     int a = fibousingrec(i);
+        //     System.out.print(a+" ");
+        // }
     }
     static void fibo(int n){
         int a = 0;
         int b = 1;
-        
         for(int i=0;i<n;i++){
             System.out.print(a+" ");
             int temp = a+b;
--- insert0atlast.java
+++ insert0atlast.java
@@ -2,16 +2,15 @@ import java.util.Arrays;
 
 public class insert0atlast {
     public static void main(String[] args) {
-        int[] arr = {0,1,0,2,3,4,0,4,0,9,9,4,1};
-        int n = arr.length;
+        int[] arr = {0,1,0,2,3,4,0,4,0,9,9,0,1};
         int index = 0;
-        for(int i=0;i<n;i++){
+        for(int i = 0;i<arr.length;i++){
             if(arr[i]!=0){
-                arr[index]=arr[i];
+                arr[index] = arr[i];
                 index++;
             }
         }
-        while(index<n){
+        while(index<arr.length){
             arr[index]=0;
             index++;
         }
