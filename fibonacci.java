ed9ef4eacbabc4545d7e5ed6aafda4467b82574f
shreyaslande <shreyaslande200@gmail.com>
2026-03-11 18:45:56 +0530

    fibonacci

--- fibonacci.java
+++ fibonacci.java
@@ -5,18 +5,29 @@ public class fibonacci {
         Scanner in = new Scanner(System.in);
         System.out.print("enter the number you want to print fibonacci series up to that number: ");
         int n = in.nextInt();
-        fibo(n);
+        // fibo(n);
+
+        //fibonacci series using reccursioon
+        for(int i=0;i<n;i++){
+            int a = fibousingrec(i);
+            System.out.print(a+" ");
+        }
     }
     static void fibo(int n){
+        int a = 0;
+        int b = 1;
         
         for(int i=0;i<n;i++){
-            if(i==0){
-                System.out.print(0+" ");
-            }else if(i==1){
-                System.out.print(1+" ");
-            }else{
-                System.out.print((i-2)+(i-1)+" ");
-            }
+            System.out.print(a+" ");
+            int temp = a+b;
+            a = b;
+            b = temp;
         }
+    }
+    static int fibousingrec(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        return (fibousingrec(n-2)+fibousingrec(n-1));
     }
 }
